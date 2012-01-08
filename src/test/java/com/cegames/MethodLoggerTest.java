f5a121ff01b64811a81787b9ffbaaecbbd978b2a
David Ross Drake <yurodivuie@gmail.com>
2012-01-08 14:39:47 -0800

    Added better tests for logging

--- src/test/java/com/cegames/MethodLoggerTest.java
+++ src/test/java/com/cegames/MethodLoggerTest.java
@@ -1,13 +1,17 @@
 package com.cegames;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.Appender;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.Description;
+import org.mockito.ArgumentCaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -31,6 +35,7 @@ public class MethodLoggerTest {
         MethodLogger defaultMethodLogger = new MethodLogger();
 
         Appender<ILoggingEvent> mockAppender = mock(Appender.class);
+        ArgumentCaptor<ILoggingEvent> argument = ArgumentCaptor.forClass(ILoggingEvent.class);
 
         try {
             ((ch.qos.logback.classic.Logger) log).addAppender(mockAppender);
@@ -42,14 +47,16 @@ public class MethodLoggerTest {
 
             defaultMethodLogger.starting(description);
 
-            verify(mockAppender).doAppend(any(ILoggingEvent.class));
+            verify(mockAppender).doAppend(argument.capture());
+            assertThat(argument.getValue().getLoggerName(), equalToIgnoringCase(MethodLoggerTest.class.getName()));
+            assertThat(argument.getValue().getMessage(), equalToIgnoringCase(MethodLogger.DEFAULT_FORMAT));
         } finally {
             ((ch.qos.logback.classic.Logger) log).detachAppender(mockAppender);
         }
     }
     
     @Test
-    public void testLoggerConstructor() {
+    public void testLoggerOnlyConstructor() {
         String methodName = "testMethod";
         Logger mlog = mock(Logger.class);
         Description description = mock(Description.class);
@@ -62,9 +69,9 @@ public class MethodLoggerTest {
     }
 
     @Test
-    public void testLoggerPrefixConstructor() {
+    public void testLoggerFormatConstructor() {
         String methodName = "anotherTest";
-        String testFormat = "<{}> - <{}>!!!!";
+        String testFormat = "TEST - <{}>!!!!";
         Logger mlog = mock(Logger.class);
         Description description = mock(Description.class);
         when(description.getMethodName()).thenReturn(methodName);
@@ -73,5 +80,34 @@ public class MethodLoggerTest {
         testMethodLogger.starting(description);
 
         verify(mlog).info(testFormat, methodName);
+    }
+
+    @Test
+    public void testFormatOnlyConstructor() {
+        String testFormat = "Fancy test started ~@~ +|{}|+";
+        String methodName = "mockMethod";
+        
+        MethodLogger defaultMethodLogger = new MethodLogger(testFormat);
+
+        Appender<ILoggingEvent> mockAppender = mock(Appender.class);
+        ArgumentCaptor<ILoggingEvent> argument = ArgumentCaptor.forClass(ILoggingEvent.class);
+
+        try {
+            ((ch.qos.logback.classic.Logger) log).addAppender(mockAppender);
+
+            Description description = mock(Description.class);
+
+            when(description.getClassName()).thenReturn(MethodLoggerTest.class.getName());
+            when(description.getMethodName()).thenReturn(methodName);
+
+            defaultMethodLogger.starting(description);
+
+            verify(mockAppender).doAppend(argument.capture());
+
+            assertThat(argument.getValue().getLoggerName(), equalToIgnoringCase(MethodLoggerTest.class.getName()));
+            assertThat(argument.getValue().getMessage(), equalToIgnoringCase(testFormat));
+        } finally {
+            ((ch.qos.logback.classic.Logger) log).detachAppender(mockAppender);
+        }
     }
 }
